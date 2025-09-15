f59062c673ceac4c8c3811e54148961941635eea
Francesco Napolitano <francesco.46napolitano@gmail.com>
2025-09-15 12:37:33 +0200

    register and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,14 @@ import { LaptopComponent } from './componenti/laptop/laptop.component';
 import { NotfoundComponent } from './componenti/notfound/notfound.component';
 
 import { ProfileComponent } from './componenti/profile/profile.component';
+import { RegisterComponent } from './componenti/register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'home'},
+  {path: 'register' , component:RegisterComponent},
+  {path: 'login' , component:LoginComponent},
   {path:'home', component:HomeComponent},
   {path:'chisiamo', component:ChisiamoComponent},
   {path: 'carrello', component:CarrelloComponent},
