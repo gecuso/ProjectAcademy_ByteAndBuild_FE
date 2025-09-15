31d1332a38c3199babb7f3539e60dbecbb70ffb4
exnomis <exnomis@gmail.com>
2025-09-15 10:21:45 +0200

    commit

--- src/app/componenti/dettaglio-prodotto/dettaglio-prodotto.component.ts
+++ src/app/componenti/dettaglio-prodotto/dettaglio-prodotto.component.ts
@@ -20,7 +20,7 @@ export class DettaglioProdottoComponent {
       descrizione: 'Intel i3, 8GB RAM, 128GB SSD',
       marca: 'HP',
       prezzo: 1000,
-      disponibilita: 12,
+      disponibilita: 0,
       immagine:
         'https://i5.walmartimages.com/seo/HP-Pavilion-13-3-FHD-Intel-Core-i3-8GB-RAM-128GB-SSD-Silver_906cf222-d138-430a-8146-d129b0cca3a2_2.f838f300a6e31f50074faf4091a1da7b.jpeg',
     };
