1a3678be5be5b3d72943ca37188218aadd58b776
exnomis <exnomis@gmail.com>
2025-09-22 16:07:37 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './componenti/home/home.component';
 import { ChisiamoComponent } from './componenti/footer/chisiamo/chisiamo.component';
 import { LavoraConNoiComponent } from './componenti/lavora-con-noi/lavora-con-noi.component';
 import { CarrelloComponent } from './componenti/carrello/carrello.component';
-import { LaptopComponent } from './componenti/laptop/laptop.component';
 import { NotfoundComponent } from './componenti/notfound/notfound.component';
 import { DettaglioProdottoComponent } from './componenti/dettaglio-prodotto/dettaglio-prodotto.component';
 import { ProfileComponent } from './componenti/profile/profile.component';
@@ -24,7 +23,6 @@ const routes: Routes = [
   {path:'chisiamo', component:ChisiamoComponent},
   {path:'lavoraconnoi', component:LavoraConNoiComponent},
   {path: 'carrello', component:CarrelloComponent},
-  {path: 'laptop', component:LaptopComponent},
   {path:'404', component:NotfoundComponent},
   {path:'profile', component:ProfileComponent},
   {path:'dettagliUtente', component:DettagliUtenteComponent, canActivate:[authGuard]},
