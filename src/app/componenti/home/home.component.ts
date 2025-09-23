35955ceacdab07d8fe287c8e59f209930b5a005f
exnomis <exnomis@gmail.com>
2025-09-23 17:07:26 +0200

    update Home pc in evidenza

--- src/app/componenti/home/home.component.ts
+++ src/app/componenti/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ListaProdottiService } from '../../services/lista-prodotti.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,22 +8,22 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  pcs: any[] = []; // Solo PC
+
   constructor(
     private route: ActivatedRoute,
     private listaProdottiService: ListaProdottiService
   ) {}
-  prodotti: any[] = [];
 
   ngOnInit(): void {
-    this.listaProdottiService.getAll().subscribe((resp: any) => {
-      this.prodotti = resp.dati;
-    });
-  }
-
+  this.listaProdottiService.getAllByIdCategoria(13).subscribe((resp: any) => {
+    this.pcs = resp.dati;
+  });
+}
   capitalize(text: string): string {
-    //funzione per mettere la prima lettera MAIUSCOLA
     if (!text) return '';
     return text.charAt(0).toUpperCase() + text.slice(1);
   }
+
 }
