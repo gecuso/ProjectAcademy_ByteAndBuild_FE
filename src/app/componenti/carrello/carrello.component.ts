6dee5ebe9bb49800ce03661cb7bdff4ae3c47ca1
gecuso <borcamattia@gmail.com>
2025-09-30 11:59:01 +0200

    inizio implementazione service per carrello

--- src/app/componenti/carrello/carrello.component.ts
+++ src/app/componenti/carrello/carrello.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CarrelloService } from '../../services/carrello.service';
 import { Subscription } from 'rxjs';
+import { OggettoNelCarrelloReq } from '../../requests/general-req/general-req.component';
 
 @Component({
   selector: 'app-carrello',
@@ -12,17 +13,19 @@ export class CarrelloComponent {
 
   //serve per connettersi al service
   private subscription: Subscription = new Subscription();
+  oggettoNelCarrello: OggettoNelCarrelloReq[] = [];
 
   //costruttore per usare il service
   constructor (
-    private CarrelloService: CarrelloService
+    private carrelloService: CarrelloService,
   ) {}
-
-  oggettoNelCarrello = [
-    { id: 1, nome: 'Intel i9', quantita: 2, immagine: 'aaa' },
-    { id: 2, nome: 'Intel i7', quantita: 1, immagine: 'aaa' },
-    { id: 3, nome: 'Intel i5', quantita: 3, immagine: 'aaa' }
-  ];
+  
+  ngOnInit(): void {
+    const idCarrello = 1; // al momento fisso
+    this.subscription = this.carrelloService.listByIdCarrello(idCarrello).subscribe((data: any) => {
+      this.oggettoNelCarrello = data;
+    });
+  }
 
   prodotti = [
     { nome: 'Intel i9', prezzo: 4.5 },
@@ -42,7 +45,7 @@ export class CarrelloComponent {
   rimuoviDalCarrello(id: number): void {
     this.oggettoNelCarrello = this.oggettoNelCarrello.filter(item => item.id !== id);
   }
-
+  /*
   getTotalePrezzo(): number {
     return this.oggettoNelCarrello.reduce((tot, item) => {
       const prodotto = this.prodotti.find(p => p.nome === item.nome);
@@ -54,16 +57,9 @@ export class CarrelloComponent {
     const item = this.oggettoNelCarrello.find(p => p.nome === nomeProdotto);
     return item ? item.quantita : 1;  // se non trovato, ritorna 1 per mostrare solo il prezzo senza quantità
   }
-
+  */
   // metodi_______________________________________________________________________
 
-  ngOnInit(): void {
-    const idCarrello = 1; // al momento fisso
-
-    this.subscription = this.CarrelloService.listByIdCarrello(idCarrello).subscribe((data: any) => {
-      this.oggettoNelCarrello = data;
-    });
-  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
